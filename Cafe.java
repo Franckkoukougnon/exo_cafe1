bcd589e3c550584203b6a20b8978a59cf741afbb
franck <franckkoukougnon@yahoo.fr>
2023-01-11 14:23:58 +0100

    exo java Café

---
+++ Cafe.java
@@ -0,0 +1,17 @@
+package cafetiere.correction.exo4;
+
+public class Cafe {
+ 
+	TypeCafe typeCafe  ; 
+	double quantiteLiquideMl ; 
+	
+	Cafe() {
+		typeCafe = TypeCafe.MOKA ;
+		quantiteLiquideMl = 100 ; 
+	}
+	
+	Cafe(TypeCafe typeCafe, double quantiteLiquideMl) {
+		this.typeCafe = typeCafe ; 
+		this.quantiteLiquideMl = quantiteLiquideMl ; 
+	}
+}
---
+++ TypeCafe.java
@@ -0,0 +1,11 @@
+package cafetiere.correction.exo4;
+
+public enum TypeCafe {
+
+	JAVA,
+	MOKA,
+	TYPICA,
+	BOURBON,
+	BATARD
+	
+}
